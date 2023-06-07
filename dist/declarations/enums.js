125fc04afbd07c017f738e6ee478b940fd0940d2
Emam546 <emam54637@gmail.com>
2023-06-07 06:32:11 +0200

    delete dist file

--- dist/declarations/enums.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.NodeEnvs = void 0;
-var NodeEnvs;
-(function (NodeEnvs) {
-    NodeEnvs["Dev"] = "development";
-    NodeEnvs["Staging"] = "staging";
-    NodeEnvs["Production"] = "production";
-})(NodeEnvs = exports.NodeEnvs || (exports.NodeEnvs = {}));
