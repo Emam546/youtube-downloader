83511a266606e8f3c6a9b5e373e21c418d6b9ed3
Emam546 <emam54637@gmail.com>
2024-09-13 04:54:44 +0300

    fix start Downloading problem

--- app/main/lib/progressBar/utils/index.ts
+++ app/main/lib/progressBar/utils/index.ts
@@ -1,5 +1,4 @@
 import { powerSaveBlocker } from "electron";
-
 import { Transform, TransformCallback, TransformOptions } from "stream";
 export class PowerStarter {
     private id: number | null = null;
@@ -16,8 +15,91 @@ export interface Options extends TransformOptions {
     bps: number;
     delayTime?: number;
 }
+export interface ModifiedThrottle {
+    addListener(event: "close", listener: () => void): this;
+    addListener(event: "data", listener: (chunk: any) => void): this;
+    addListener(event: "end", listener: () => void): this;
+    addListener(event: "error", listener: (err: Error) => void): this;
+    addListener(event: "pause", listener: () => void): this;
+    addListener(event: "readable", listener: () => void): this;
+    addListener(event: "resume", listener: () => void): this;
+    addListener(event: "reset-speed", listener: () => void): this;
+    addListener(
+        event: string | symbol,
+        listener: (...args: any[]) => void
+    ): this;
 
+    emit(event: "close"): boolean;
+    emit(event: "data", chunk: any): boolean;
+    emit(event: "end"): boolean;
+    emit(event: "error", err: Error): boolean;
+    emit(event: "pause"): boolean;
+    emit(event: "readable"): boolean;
+    emit(event: "resume"): boolean;
+    emit(event: "reset-speed", listener: () => void): this;
+    emit(event: string | symbol, ...args: any[]): boolean;
+
+    on(event: "close", listener: () => void): this;
+    on(event: "data", listener: (chunk: any) => void): this;
+    on(event: "end", listener: () => void): this;
+    on(event: "error", listener: (err: Error) => void): this;
+    on(event: "pause", listener: () => void): this;
+    on(event: "readable", listener: () => void): this;
+    on(event: "resume", listener: () => void): this;
+    on(event: "reset-speed", listener: () => void): this;
+    on(event: string | symbol, listener: (...args: any[]) => void): this;
+
+    once(event: "close", listener: () => void): this;
+    once(event: "data", listener: (chunk: any) => void): this;
+    once(event: "end", listener: () => void): this;
+    once(event: "error", listener: (err: Error) => void): this;
+    once(event: "pause", listener: () => void): this;
+    once(event: "readable", listener: () => void): this;
+    once(event: "resume", listener: () => void): this;
+    once(event: "reset-speed", listener: () => void): this;
+    once(event: string | symbol, listener: (...args: any[]) => void): this;
+
+    prependListener(event: "close", listener: () => void): this;
+    prependListener(event: "data", listener: (chunk: any) => void): this;
+    prependListener(event: "end", listener: () => void): this;
+    prependListener(event: "error", listener: (err: Error) => void): this;
+    prependListener(event: "pause", listener: () => void): this;
+    prependListener(event: "readable", listener: () => void): this;
+    prependListener(event: "resume", listener: () => void): this;
+    prependListener(event: "reset-speed", listener: () => void): this;
+    prependListener(
+        event: string | symbol,
+        listener: (...args: any[]) => void
+    ): this;
+
+    prependOnceListener(event: "close", listener: () => void): this;
+    prependOnceListener(event: "data", listener: (chunk: any) => void): this;
+    prependOnceListener(event: "end", listener: () => void): this;
+    prependOnceListener(event: "error", listener: (err: Error) => void): this;
+    prependOnceListener(event: "pause", listener: () => void): this;
+    prependOnceListener(event: "readable", listener: () => void): this;
+    prependOnceListener(event: "resume", listener: () => void): this;
+    prependOnceListener(event: "reset-speed", listener: () => void): this;
+    prependOnceListener(
+        event: string | symbol,
+        listener: (...args: any[]) => void
+    ): this;
+
+    removeListener(event: "close", listener: () => void): this;
+    removeListener(event: "data", listener: (chunk: any) => void): this;
+    removeListener(event: "end", listener: () => void): this;
+    removeListener(event: "error", listener: (err: Error) => void): this;
+    removeListener(event: "pause", listener: () => void): this;
+    removeListener(event: "readable", listener: () => void): this;
+    removeListener(event: "resume", listener: () => void): this;
+    removeListener(event: "reset-speed", listener: () => void): this;
+    removeListener(
+        event: string | symbol,
+        listener: (...args: any[]) => void
+    ): this;
+}
 export class ModifiedThrottle extends Transform {
+    private startDownloading: boolean = false;
     private isPausedManually: boolean = false;
     private pauseTimeout: NodeJS.Timeout | null = null;
     private delayTime: number;
@@ -66,6 +148,8 @@ export class ModifiedThrottle extends Transform {
         encoding: BufferEncoding,
         callback: TransformCallback
     ): void {
+        if (!this.startDownloading) this.emit("reset-speed");
+        this.startDownloading = true;
         const sendData = (chunk: Buffer) => {
             return new Promise<boolean>((resolve) => {
                 this.totalBytes += chunk.length;
