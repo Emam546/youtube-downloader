f52aa071f6d782a9e13693ad63b9eaeddeeda4ea
Emam546 <emam54637@gmail.com>
2024-09-15 02:40:29 +0300

    gix version problem

--- app/main/updater/AppUpdater.ts
+++ app/main/updater/AppUpdater.ts
@@ -129,7 +129,7 @@ export default class AppUpdater extends EventEmitter {
       `https://api.github.com/repos/${this.data.owner}/${this.data.repo}/releases`
     );
     const update = response.data.find((release) => {
-      return semver.gtr(app.getVersion(), release.tag_name);
+      return !semver.gte(app.getVersion(), release.tag_name);
     });
     if (!update) return this.emit("update-not-available");
     this.emit("update-available", update);
