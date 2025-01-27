9ff8a76b78b2b6f7a3b4df6207234b28151fc876
Emam546 <emam54637@gmail.com>
2025-01-27 20:19:18 +0200

    fix controls time text input

--- src/components/Youtube/youtubeViewer/controls.tsx
+++ src/components/Youtube/youtubeViewer/controls.tsx
@@ -1,276 +1,274 @@
 import {
-    faCheck,
-    faPause,
-    faPlay,
-    faRotateLeft,
-    faRotateRight,
-    faXmark,
+  faCheck,
+  faPause,
+  faPlay,
+  faRotateLeft,
+  faRotateRight,
+  faXmark,
 } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
-    convertMMSSToSeconds,
-    convertSecondsToMMSS,
-    isValidMMSSFormat,
+  convertMMSSToSeconds,
+  convertSecondsToHHMMSS,
+  isValidHHMMSSFormat,
 } from "@src/utils";
 import classNames from "classnames";
 import React, { ComponentProps, useEffect, useState } from "react";
+import { MIN_TIME } from "./range";
 interface Props {
-    aspect: AspectsType;
-    setAspect?: (state: AspectsType) => any;
-    duration: number;
-    start: number;
-    end: number;
-    onSeek?: (second: number) => any;
-    onSetLoop?: (state: boolean) => any;
-    curTime: number;
-    loop: boolean;
-    play: boolean;
-    onSetState?: (state: boolean) => any;
-    onDuration?: (start: number, end: number) => any;
+  aspect: AspectsType;
+  setAspect?: (state: AspectsType) => any;
+  duration: number;
+  start: number;
+  end: number;
+  onSeek?: (second: number) => any;
+  onSetLoop?: (state: boolean) => any;
+  curTime: number;
+  loop: boolean;
+  play: boolean;
+  onSetState?: (state: boolean) => any;
+  onDuration?: (start: number, end: number) => any;
 }
 export function ControlButton({
-    className,
-    ...props
+  className,
+  ...props
 }: ComponentProps<"button">) {
-    return (
-        <button
-            className={classNames(
-                "flex tw-items-center tw-justify-center tw-w-12 tw-h-12 tw-bg-blue-500 hover:tw-bg-blue-600",
-                className
-            )}
-            {...props}
-        />
-    );
+  return (
+    <button
+      className={classNames(
+        "flex tw-items-center tw-justify-center tw-w-12 tw-h-12 tw-bg-blue-500 hover:tw-bg-blue-600",
+        className
+      )}
+      {...props}
+    />
+  );
 }
 interface TimeGetterProps {
-    label: string;
-    id: string;
-    time: number;
-    onTime: (time: number) => any;
+  label: string;
+  id: string;
+  time: number;
+  onTime: (time: number) => any;
 }
-export function FanyInput({ className, ...props }: ComponentProps<"input">) {
-    return (
-        <div
-            className={classNames(
-                "tw-relative",
-                (className =
-                    "before:tw-contents-[''] before:tw-w-0 before:tw-duration-200 before:tw-transition-[width] before:tw-absolute before:tw-bottom-0 before:focus-within:tw-w-full before:tw-left-[50%] before:-tw-translate-x-[50%] before:tw-h-[2px] before:tw-z-10 before:tw-bg-blue-500")
-            )}
-        >
-            <input
-                {...props}
-                className={classNames(
-                    "tw-px-2 tw-w-[4rem] tw-py-1  tw-relative tw-border-b-2 tw-border-gray-600 focus:tw-outline-none hover:tw-outline-none",
-                    className
-                )}
-            />
-            <span></span>
-        </div>
-    );
+export function FancyInput({ className, ...props }: ComponentProps<"input">) {
+  return (
+    <div
+      className={classNames(
+        "tw-relative",
+        "before:tw-contents-[''] before:tw-w-0 before:tw-duration-200 before:tw-transition-[width] before:tw-absolute before:tw-bottom-0 before:focus-within:tw-w-full before:tw-left-[50%] before:-tw-translate-x-[50%] before:tw-h-[2px] before:tw-z-10 before:tw-bg-blue-500"
+      )}
+    >
+      <input
+        {...props}
+        className={classNames(
+          "tw-bg-transparent tw-px-2 tw-w-[4rem] tw-py-1 tw-border-b-2 tw-border-gray-600 focus:tw-outline-none hover:tw-outline-none",
+          className
+        )}
+      />
+    </div>
+  );
 }
 function TimeGetter({ label, id, time, onTime }: TimeGetterProps) {
-    const [val, setVal] = useState(convertSecondsToMMSS(time));
-    useEffect(() => {
-        setVal(convertSecondsToMMSS(time));
-    }, [time]);
-    return (
-        <div className="tw-flex tw-items-center tw-gap-x-1">
-            <label htmlFor={id}>{label}</label>
-            <FanyInput
-                id={id}
-                onBlur={() => {}}
-                type="text"
-                onChange={(e) => {
-                    setVal(e.currentTarget.value);
-                }}
-                onBlurCapture={() => {
-                    if (isValidMMSSFormat(val))
-                        onTime(convertMMSSToSeconds(val));
-                }}
-                value={val}
-            />
-        </div>
-    );
+  const [val, setVal] = useState(convertSecondsToHHMMSS(time));
+  useEffect(() => {
+    setVal(convertSecondsToHHMMSS(time));
+  }, [time]);
+  return (
+    <div className="tw-flex tw-items-center tw-gap-x-1">
+      <label htmlFor={id}>{label}</label>
+      <div className="tw-flex-shrink-0">
+        <FancyInput
+          className="tw-min-w-[6rem]"
+          id={id}
+          onBlur={() => {}}
+          type="text"
+          onChange={(e) => {
+            setVal(e.currentTarget.value);
+          }}
+          onBlurCapture={() => {
+            if (isValidHHMMSSFormat(val) && convertMMSSToSeconds(val) >= 0)
+              onTime(convertMMSSToSeconds(val));
+            else setVal(convertSecondsToHHMMSS(time));
+          }}
+          value={val}
+        />
+      </div>
+    </div>
+  );
 }
 interface LengthProps {
-    start: number;
-    end: number;
+  start: number;
+  end: number;
 }
 function LengthComp({ start, end }: LengthProps) {
-    return (
-        <div>
-            Length:
-            <span> {convertSecondsToMMSS(end - start)}</span>
-        </div>
-    );
+  return (
+    <div>
+      Length:
+      <span> {convertSecondsToHHMMSS(end - start)}</span>
+    </div>
+  );
 }
 interface LoopProps {
-    loop: boolean;
-    setLoop: (state: boolean) => any;
+  loop: boolean;
+  setLoop: (state: boolean) => any;
 }
 function LoopComp({ loop, setLoop }: LoopProps) {
-    return (
-        <div className="tw-items-center tw-flex">
-            <span>Loop: </span>
+  return (
+    <div className="tw-items-center tw-flex">
+      <span>Loop: </span>
 
-            <button
-                onClick={() => {
-                    setLoop(!loop);
-                }}
-                className="tw-text-xl"
-                type="button"
-            >
-                {loop ? (
-                    <FontAwesomeIcon icon={faCheck} />
-                ) : (
-                    <FontAwesomeIcon icon={faXmark} />
-                )}
-            </button>
-        </div>
-    );
+      <button
+        onClick={() => {
+          setLoop(!loop);
+        }}
+        className="tw-text-xl"
+        type="button"
+      >
+        {loop ? (
+          <FontAwesomeIcon icon={faCheck} />
+        ) : (
+          <FontAwesomeIcon icon={faXmark} />
+        )}
+      </button>
+    </div>
+  );
 }
 export type AspectsType = "16:9" | "4:3";
 export function FancyRadio({
-    id,
-    children,
-    className,
-    checked,
-    ...props
+  id,
+  children,
+  className,
+  checked,
+  ...props
 }: ComponentProps<"input">) {
-    return (
-        <div>
-            <label
-                aria-checked={checked}
-                className={classNames(
-                    "tw-px-3 tw-py-2.5 aria-checked:tw-bg-gray-500 aria-checked:tw-text-gray-50 tw-select-none tw-cursor-pointer"
-                )}
-                htmlFor={id}
-            >
-                {children}
-            </label>
-            <input
-                id={id}
-                {...props}
-                className={classNames(
-                    className,
-                    "tw-appearance-none tw-absolute"
-                )}
-                type="radio"
-            />
-        </div>
-    );
+  return (
+    <div>
+      <label
+        aria-checked={checked}
+        className={classNames(
+          "tw-px-3 tw-py-2.5 aria-checked:tw-bg-gray-500 aria-checked:tw-text-gray-50 tw-select-none tw-cursor-pointer"
+        )}
+        htmlFor={id}
+      >
+        {children}
+      </label>
+      <input
+        id={id}
+        {...props}
+        className={classNames(className, "tw-appearance-none tw-absolute")}
+        type="radio"
+      />
+    </div>
+  );
 }
 export interface AspectSwitcherProps {
-    state: AspectsType;
-    setState?: (state: AspectsType) => any;
+  state: AspectsType;
+  setState?: (state: AspectsType) => any;
 }
 export function AspectSwitcher({ state, setState }: AspectSwitcherProps) {
-    return (
-        <div className="tw-flex tw-overflow-hidden tw-rounded">
-            <FancyRadio
-                onChange={() => setState?.("16:9")}
-                checked={state == "16:9"}
-                name="aspect"
-                id="aspect-16:9"
-            >
-                16:9
-            </FancyRadio>
-            <FancyRadio
-                name="aspect"
-                onChange={() => setState?.("4:3")}
-                checked={state == "4:3"}
-                id="aspect-4:3"
-            >
-                4:3
-            </FancyRadio>
-        </div>
-    );
+  return (
+    <div className="tw-flex tw-overflow-hidden tw-rounded">
+      <FancyRadio
+        onChange={() => setState?.("16:9")}
+        checked={state == "16:9"}
+        name="aspect"
+        id="aspect-16:9"
+      >
+        16:9
+      </FancyRadio>
+      <FancyRadio
+        name="aspect"
+        onChange={() => setState?.("4:3")}
+        checked={state == "4:3"}
+        id="aspect-4:3"
+      >
+        4:3
+      </FancyRadio>
+    </div>
+  );
 }
 export default function Controls({
-    aspect: switcher,
-    setAspect: setSwitcher,
-    duration,
-    end,
-    loop,
-    play,
-    start,
-    curTime,
-    onDuration,
-    onSeek,
-    onSetLoop,
-    onSetState,
+  aspect: switcher,
+  setAspect: setSwitcher,
+  duration,
+  end,
+  loop,
+  play,
+  start,
+  curTime,
+  onDuration,
+  onSeek,
+  onSetLoop,
+  onSetState,
 }: Props) {
-    return (
-        <div className="tw-flex tw-justify-between tw-flex-wrap tw-gap-y-4 tw-gap-x-2">
-            <div className="tw-flex tw-items-center tw-gap-x-4">
-                <div className="tw-flex tw-rounded tw-overflow-hidden tw-text-gray-100 tw-text-lg">
-                    <ControlButton
-                        title="Play"
-                        onClick={() => {
-                            onSetState?.(!play);
-                        }}
-                    >
-                        {play ? (
-                            <FontAwesomeIcon icon={faPause} />
-                        ) : (
-                            <FontAwesomeIcon icon={faPlay} />
-                        )}
-                    </ControlButton>
-                    <ControlButton
-                        title="jumb to start selection"
-                        onClick={() => onSeek?.(start)}
-                    >
-                        <FontAwesomeIcon icon={faRotateLeft} />
-                    </ControlButton>
-                    <ControlButton
-                        onClick={() => onSeek?.(end - 5)}
-                        title="jumb to end selection"
-                    >
-                        <FontAwesomeIcon icon={faRotateRight} />
-                    </ControlButton>
-                </div>
-                <div>
-                    <AspectSwitcher
-                        state={switcher}
-                        setState={setSwitcher}
-                    />
-                </div>
-                <p className="text-lg">
-                    <span>{convertSecondsToMMSS(curTime)}</span>
-                    {` / `}
-                    <span>{convertSecondsToMMSS(duration)}</span>
-                </p>
-            </div>
-            <div className="tw-flex tw-gap-x-2 tw-items-center tw-flex-wrap tw-text-lg tw-gap-y-4">
-                <div className="tw-flex tw-gap-x-2 tw-items-center">
-                    <TimeGetter
-                        id="start-input"
-                        label="Start:"
-                        onTime={(time) => {
-                            onDuration?.(time, end);
-                        }}
-                        time={start}
-                    />
-                    <TimeGetter
-                        id="end-input"
-                        label="End:"
-                        onTime={(time) => {
-                            onDuration?.(start, time);
-                        }}
-                        time={end}
-                    />
-                </div>
-                <div className="tw-flex tw-gap-x-2 tw-items-center">
-                    <LengthComp
-                        start={start}
-                        end={end}
-                    />
-                    <LoopComp
-                        loop={loop}
-                        setLoop={(state) => onSetLoop?.(state)}
-                    />
-                </div>
-            </div>
+  return (
+    <div className="tw-flex tw-justify-between tw-flex-wrap tw-gap-y-4 tw-gap-x-2">
+      <div className="tw-flex tw-items-center tw-gap-x-4">
+        <div className="tw-flex tw-rounded tw-overflow-hidden tw-text-gray-100 tw-text-lg">
+          <ControlButton
+            title="Play"
+            onClick={() => {
+              onSetState?.(!play);
+            }}
+          >
+            {play ? (
+              <FontAwesomeIcon icon={faPause} />
+            ) : (
+              <FontAwesomeIcon icon={faPlay} />
+            )}
+          </ControlButton>
+          <ControlButton
+            title="jumb to start selection"
+            onClick={() => onSeek?.(start)}
+          >
+            <FontAwesomeIcon icon={faRotateLeft} />
+          </ControlButton>
+          <ControlButton
+            onClick={() => onSeek?.(end - 5)}
+            title="jumb to end selection"
+          >
+            <FontAwesomeIcon icon={faRotateRight} />
+          </ControlButton>
         </div>
-    );
+        <div>
+          <AspectSwitcher state={switcher} setState={setSwitcher} />
+        </div>
+        <p className="text-lg">
+          <span>{convertSecondsToHHMMSS(curTime)}</span>
+          {` / `}
+          <span>{convertSecondsToHHMMSS(duration)}</span>
+        </p>
+      </div>
+      <div className="tw-flex tw-gap-x-2 tw-items-center tw-flex-wrap tw-text-lg tw-gap-y-4">
+        <div className="tw-flex tw-gap-x-2 tw-items-center">
+          <div>
+            <TimeGetter
+              id="start-input"
+              label="Start:"
+              onTime={(time) => {
+                onDuration?.(Math.max(0, Math.min(time, end - MIN_TIME)), end);
+              }}
+              time={start}
+            />
+          </div>
+          <div>
+            <TimeGetter
+              id="end-input"
+              label="End:"
+              onTime={(time) => {
+                onDuration?.(
+                  start,
+                  Math.max(start + MIN_TIME, Math.min(time, duration))
+                );
+              }}
+              time={end}
+            />
+          </div>
+        </div>
+        <div className="tw-flex tw-gap-x-2 tw-items-center">
+          <LengthComp start={start} end={end} />
+          <LoopComp loop={loop} setLoop={(state) => onSetLoop?.(state)} />
+        </div>
+      </div>
+    </div>
+  );
 }
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,26 +1,28 @@
-export function convertSecondsToMMSS(seconds: number) {
-  // Calculate minutes and seconds
-  const minutes = Math.floor(seconds / 60);
-  const remainingSeconds = Math.floor(seconds % 60);
+export function convertSecondsToHHMMSS(durationInSeconds: number) {
+  const hours = Math.floor(durationInSeconds / 3600);
+  const minutes = Math.floor((durationInSeconds % 3600) / 60);
+  const seconds = Math.floor(durationInSeconds % 60);
 
-  // Format with leading zeros if necessary
-  const formattedMinutes = String(minutes).padStart(2, "0");
-  const formattedSeconds = String(remainingSeconds).padStart(2, "0");
+  // Pad with leading zeros if necessary
+  const formattedHours = hours.toString().padStart(2, "0");
+  const formattedMinutes = minutes.toString().padStart(2, "0");
+  const formattedSeconds = seconds.toString().padStart(2, "0");
 
-  return `${formattedMinutes}:${formattedSeconds}`;
+  return `${formattedHours}:${formattedMinutes}:${formattedSeconds}`;
 }
-export function isValidMMSSFormat(timeString: string) {
+
+export function isValidHHMMSSFormat(timeString: string) {
   // Regex to match MM:SS format, where MM and SS are two digits each
-  const regex = /^\d{2,10}:\d{2}$/;
-  return regex.test(timeString);
+  const timeRegex = /^([0-9]{2}):([0-5][0-9]):([0-5][0-9])$/;
+  return timeRegex.test(timeString);
 }
 export function convertMMSSToSeconds(timeString: string) {
-  if (!isValidMMSSFormat(timeString)) {
+  if (!isValidHHMMSSFormat(timeString)) {
     throw new Error("Invalid time format. Expected MM:SS format.");
   }
 
-  const [minutes, seconds] = timeString.split(":").map(Number);
-  return minutes * 60 + seconds;
+  const [hours, minutes, seconds] = timeString.split(":").map(Number);
+  return hours * 60 * 60 + minutes * 60 + seconds;
 }
 export function isValidUrl(string: string) {
   try {
