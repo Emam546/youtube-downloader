f84dd6bf7ce1fce0084c808b39264ce73bf28805
Emam546 <emam54637@gmail.com>
2025-02-12 16:34:04 +0200

    fix some minor issues

--- app/renderer/progress/pages/download/Systems.tsx
+++ app/renderer/progress/pages/download/Systems.tsx
@@ -3,102 +3,103 @@ import React, { ComponentProps, useEffect, useState } from "react";
 import { useAppSelector } from "../../store";
 import { humanFileSize, millisecondsToStr } from "@renderer/utils/calc";
 export interface StatusLiProps extends ComponentProps<"li"> {
-    label: string;
+  label: string;
 }
 export function StatusLi({
-    label,
-    children,
-    className,
-    ...props
+  label,
+  children,
+  className,
+  ...props
 }: StatusLiProps) {
-    return (
-        <li
-            className={classNames(className, "flex")}
-            {...props}
-        >
-            <div className="flex-shrink-0 flex-grow-0 w-[6.3rem]">
-                <p>{label}</p>
-            </div>
-            <div className="flex-1">{children}</div>
-        </li>
-    );
+  return (
+    <li className={classNames(className, "flex")} {...props}>
+      <div className="flex-shrink-0 flex-grow-0 w-[6.3rem]">
+        <p>{label}</p>
+      </div>
+      <div className="flex-1">{children}</div>
+    </li>
+  );
 }
 
 export function FileSizeStatus() {
-    const size = useAppSelector((state) => state.download.fileSize);
+  const size = useAppSelector((state) => state.download.fileSize);
 
-    return (
-        <StatusLi label="File size">
-            <p>
-                {size != undefined && humanFileSize(size)}
-                {size == undefined && "......"}
-            </p>
-        </StatusLi>
-    );
+  return (
+    <StatusLi label="File size">
+      <p>
+        {size != undefined && humanFileSize(size)}
+        {size == undefined && "......"}
+      </p>
+    </StatusLi>
+  );
 }
 export function DownloadedStatus() {
-    const { downloaded, fileSize } = useAppSelector((state) => state.download);
-    let status: string | undefined = undefined;
-    if (downloaded) {
-        status = humanFileSize(downloaded);
-        if (fileSize)
-            status = `${status} ${((downloaded / fileSize) * 100).toFixed(2)}%`;
-    }
+  const { downloaded, fileSize } = useAppSelector((state) => state.download);
+  let status: string | undefined = undefined;
+  if (downloaded) {
+    status = humanFileSize(downloaded);
+    if (fileSize)
+      status = `${status} ${Math.min(
+        100,
+        (downloaded / fileSize) * 100
+      ).toFixed(2)}%`;
+  }
 
-    return (
-        <StatusLi label="Downloaded">
-            <p>
-                {status != undefined && status}
-                {status == undefined && "......"}
-            </p>
-        </StatusLi>
-    );
+  return (
+    <StatusLi label="Downloaded">
+      <p>
+        {status != undefined && status}
+        {status == undefined && "......"}
+      </p>
+    </StatusLi>
+  );
 }
 export function TransferStatus() {
-    // const [speed, setSpeed] = useState<number>();
-    const speed = useAppSelector((state) => state.download.transferRate);
+  // const [speed, setSpeed] = useState<number>();
+  const speed = useAppSelector((state) => state.download.transferRate);
 
-    return (
-        <StatusLi label="Transfer rate">
-            <p>
-                {speed != undefined && humanFileSize(speed)}
-                {speed == undefined && "......"}
-            </p>
-        </StatusLi>
-    );
+  return (
+    <StatusLi label="Transfer rate">
+      <p>
+        {speed != undefined && humanFileSize(speed)}
+        {speed == undefined && "......"}
+      </p>
+    </StatusLi>
+  );
 }
 
 export function TimeLeftStatus() {
-    let status: string | undefined = undefined;
-    const { downloaded, fileSize, transferRate } = useAppSelector(
-        (state) => state.download
+  let status: string | undefined = undefined;
+  const { downloaded, fileSize, transferRate } = useAppSelector(
+    (state) => state.download
+  );
+  if (fileSize && transferRate && downloaded) {
+    const time = Math.min(
+      (fileSize - downloaded) / transferRate,
+      360 * 24 * 60 * 60
     );
-    if (fileSize && transferRate && downloaded) {
-        const time = (fileSize - downloaded) / transferRate;
-        status = millisecondsToStr(time * 1000);
-    }
-    return (
-        <StatusLi label="Time Left">
-            <p>
-                {status != undefined && `${status}`}
-                {status == undefined && "......"}
-            </p>
-        </StatusLi>
-    );
+    status = millisecondsToStr(time * 1000);
+  }
+  return (
+    <StatusLi label="Time Left">
+      <p>
+        {status != undefined && `${status}`}
+        {status == undefined && "......"}
+      </p>
+    </StatusLi>
+  );
 }
 
 export function ResumeStatus() {
-    const status = useAppSelector((state) => state.download.resumeCapacity);
+  const status = useAppSelector((state) => state.download.resumeCapacity);
 
-    return (
-        <StatusLi label="Resume capability">
-            <p className="px-4">
-                {status == true && `Yes`}
-                {status == false && (
-                    <span className="text-red-500">{"NO"}</span>
-                )}
-                {status == undefined && "......"}
-            </p>
-        </StatusLi>
-    );
+  return (
+    <StatusLi label="Resume capability">
+      <p className="px-4">
+        {status == true && `Yes`}
+        {status == false && <span className="text-red-500">{"NO"}</span>}
+        {status == undefined && "......"}
+      </p>
+    </StatusLi>
+  );
 }
--- src/components/MainComponents/Viewer/range.tsx
+++ src/components/MainComponents/Viewer/range.tsx
@@ -5,172 +5,146 @@ import { Range } from "react-range";
 
 import { IRenderTrackParams, IThumbProps } from "react-range/lib/types";
 export interface Props {
-    id: string;
-    duration: number;
-    start: number;
-    end: number;
-    setDuration(start: number, end: number): any;
+  id: string;
+  duration: number;
+  start: number;
+  end: number;
+  setDuration(start: number, end: number): any;
 }
 
 export interface ThumbProps extends Omit<IThumbProps, "ref"> {
-    isDragged: boolean;
+  isDragged: boolean;
 }
 export const Thumb = React.forwardRef<ComponentRef<"div">, ThumbProps>(
-    ({ isDragged, ...props }, ref) => {
-        return (
-            <div
-                ref={ref}
-                {...props}
-                className="tw-h-full hover:tw-outline-none focus:tw-outline-none -tw-translate-x-[50%]"
-            >
-                <div
-                    aria-grabbed={isDragged}
-                    className="tw-h-full tw-w-1 tw-bg-gray-400"
-                ></div>
-                <div
-                    aria-grabbed={isDragged}
-                    className="tw-absolute tw-top-[50%] tw-left-[50%] -tw-translate-x-[50%] -tw-translate-y-[50%] tw-bg-blue-400 tw-rounded-full tw-h-7 tw-w-7 aria-[grabbed='true']:tw-bg-blue-800"
-                ></div>
-            </div>
-        );
-    }
+  ({ isDragged, ...props }, ref) => {
+    return (
+      <div
+        ref={ref}
+        {...props}
+        className="tw-h-full hover:tw-outline-none focus:tw-outline-none -tw-translate-x-[50%]"
+      >
+        <div
+          aria-grabbed={isDragged}
+          className="tw-h-full tw-w-1 tw-bg-gray-400"
+        ></div>
+        <div
+          aria-grabbed={isDragged}
+          className="tw-absolute tw-top-[50%] tw-left-[50%] -tw-translate-x-[50%] -tw-translate-y-[50%] tw-bg-blue-400 tw-rounded-full tw-h-7 tw-w-7 aria-[grabbed='true']:tw-bg-blue-800"
+        ></div>
+      </div>
+    );
+  }
 );
 export interface TrackerProps extends IRenderTrackParams {
-    duration: number;
+  duration: number;
 }
 const MaxTiles = 3;
 interface MarkProps extends ComponentProps<"div"> {
-    skip: boolean;
-    time: number;
-    tilesNumber: number;
+  skip: boolean;
+  time: number;
+  tilesNumber: number;
 }
 export function Mark({
-    skip,
-    time,
-    tilesNumber,
-    className,
-    ...props
+  skip,
+  time,
+  tilesNumber,
+  className,
+  ...props
 }: MarkProps) {
-    return (
-        <div
-            {...props}
-            className={classNames("pb-4", className)}
-        >
-            <div className="tw-relative tw-h-3">
-                <div className="tw-absolute tw-w-0.5 tw-h-3 tw-bg-black tw-top-0 tw-left-0 -tw-translate-x-[50%]"></div>
-                <div className="tw-absolute tw-top-full tw-left-0 -tw-translate-x-[50%]">
-                    {!skip && <span className="">{time}</span>}
-                </div>
-                <div className="tw-flex tw-justify-between tw-items-start tw-w-full pt-0.5">
-                    <span></span>
-                    {new Array(Math.ceil(tilesNumber))
-                        .fill(null)
-                        .map((_, i) => {
-                            return (
-                                <span
-                                    key={`${i}`}
-                                    className="tw-w-0.5 tw-h-2 tw-bg-black -tw-translate-x-[50%]"
-                                ></span>
-                            );
-                        })}
-                    <span></span>
-                </div>
-            </div>
+  return (
+    <div {...props} className={classNames("pb-4", className)}>
+      <div className="tw-relative tw-h-3">
+        <div className="tw-absolute tw-w-0.5 tw-h-3 tw-bg-black tw-top-0 tw-left-0 -tw-translate-x-[50%]"></div>
+        <div className="tw-absolute tw-top-full tw-left-0 -tw-translate-x-[50%]">
+          {!skip && <span className="">{time}</span>}
         </div>
-    );
+        <div className="tw-flex tw-justify-between tw-items-start tw-w-full pt-0.5">
+          <span></span>
+          {new Array(Math.ceil(tilesNumber)).fill(null).map((_, i) => {
+            return (
+              <span
+                key={`${i}`}
+                className="tw-w-0.5 tw-h-2 tw-bg-black -tw-translate-x-[50%]"
+              ></span>
+            );
+          })}
+          <span></span>
+        </div>
+      </div>
+    </div>
+  );
 }
 export function Tracker({ props, children, duration }: TrackerProps) {
-    const tilesNumber = Math.max(MaxTiles - Math.floor(duration / 60 / 10), 0);
-    const skip = Math.floor(duration / 60 / 30) + 1;
-    const numTiles = Math.floor(duration / 60);
-    return (
-        <div className="tw-bg-gray-100">
-            <div
-                className="tw-h-20"
-                {...props}
-            >
-                {children}
-            </div>
-            <div className="tw-relative ">
-                <div className="tw-flex tw-min-h-10">
-                    {new Array(numTiles).fill(null).map((_, i) => {
-                        const time = Math.min(i * 60, duration);
-                        return (
-                            <Mark
-                                key={`${duration}-${i}`}
-                                skip={i % skip != 0}
-                                time={time}
-                                style={{
-                                    width: `${((1 * 60) / duration) * 100}%`,
-                                }}
-                                tilesNumber={tilesNumber}
-                            />
-                        );
-                    })}
-                    <Mark
-                        skip={numTiles % skip != 0}
-                        className="tw-flex-1 tw-w-full tw-grow"
-                        time={numTiles * 60}
-                        tilesNumber={Math.floor(
-                            ((duration - numTiles * 60) / 60) * tilesNumber
-                        )}
-                    />
-                    <Mark
-                        skip={false}
-                        time={duration}
-                        tilesNumber={0}
-                    />
-                </div>
-            </div>
+  const tilesNumber = Math.max(MaxTiles - Math.floor(duration / 60 / 10), 0);
+  const skip = Math.floor(duration / 60 / 30) + 1;
+  const numTiles = Math.floor(duration / 60);
+  return (
+    <div className="tw-bg-gray-100">
+      <div className="tw-h-20" {...props}>
+        {children}
+      </div>
+      <div className="tw-relative ">
+        <div className="tw-flex tw-min-h-10">
+          {new Array(numTiles).fill(null).map((_, i) => {
+            const time = Math.min(i * 60, duration);
+            return (
+              <Mark
+                key={`${duration}-${i}`}
+                skip={i % skip != 0}
+                time={time}
+                style={{
+                  width: `${((1 * 60) / duration) * 100}%`,
+                }}
+                tilesNumber={tilesNumber}
+              />
+            );
+          })}
+          <Mark
+            skip={numTiles % skip != 0}
+            className="tw-flex-1 tw-w-full tw-grow"
+            time={numTiles * 60}
+            tilesNumber={Math.floor(
+              ((duration - numTiles * 60) / 60) * tilesNumber
+            )}
+          />
+          <Mark skip={false} time={Math.floor(duration)} tilesNumber={0} />
         </div>
-    );
+      </div>
+    </div>
+  );
 }
 export const MIN_TIME = 5;
 export interface RangeProps {
-    duration: number;
-    start: number;
-    end: number;
-    setDuration(start: number, end: number): any;
+  duration: number;
+  start: number;
+  end: number;
+  setDuration(start: number, end: number): any;
 }
 export function RangeTracker({
-    duration,
-    start,
-    end,
-    setDuration,
+  duration,
+  start,
+  end,
+  setDuration,
 }: RangeProps) {
-    return (
-        <Range
-            step={1}
-            min={0}
-            max={duration}
-            values={[start, end]}
-            onChange={([newStart, newEnd]) => {
-                const MaxStart = Math.max(
-                    Math.min(duration, MIN_TIME),
-                    end - MIN_TIME
-                );
-                const MinEnd = Math.max(
-                    Math.min(duration, MIN_TIME),
-                    start + MIN_TIME
-                );
-                const options = [
-                    Math.min(newStart, MaxStart),
-                    Math.min(Math.max(newEnd, MinEnd), duration),
-                ] as [number, number];
-                setDuration(...options);
-            }}
-            renderTrack={({ ...props }) => (
-                <Tracker
-                    {...props}
-                    duration={duration}
-                />
-            )}
-            renderThumb={({ index, isDragged, value, props }) => (
-                <Thumb
-                    isDragged={isDragged}
-                    {...props}
-                />
-            )}
-        />
-    );
+  return (
+    <Range
+      step={1}
+      min={0}
+      max={Math.floor(duration)}
+      values={[start, end]}
+      onChange={([newStart, newEnd]) => {
+        const MaxStart = Math.max(Math.min(duration, MIN_TIME), end - MIN_TIME);
+        const MinEnd = Math.max(Math.min(duration, MIN_TIME), start + MIN_TIME);
+        const options = [
+          Math.min(newStart, MaxStart),
+          Math.min(Math.max(newEnd, MinEnd), duration),
+        ] as [number, number];
+        setDuration(...options);
+      }}
+      renderTrack={({ ...props }) => <Tracker {...props} duration={duration} />}
+      renderThumb={({ index, isDragged, value, props }) => (
+        <Thumb isDragged={isDragged} {...props} />
+      )}
+    />
+  );
 }
