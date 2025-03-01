2d76312f7e469518bae852a1716097e12cc2ffe7
Emam546 <emam54637@gmail.com>
2025-03-01 15:10:33 +0200

    update the version

--- scripts/build.js
+++ scripts/build.js
@@ -23,7 +23,7 @@ const order = {
     youtube: 0,
     link: -1000,
   },
-  version: "v0.0.4",
+  version: "v0.0.5",
 };
 
 fs.writeFileSync(path.join(folderPath, "order.json"), JSON.stringify(order));
