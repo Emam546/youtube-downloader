d65e246e2ea4b5662f4d891fe23b4e51888cd102
Emam546 <emam54637@gmail.com>
2026-09-22 12:27:04 +0300

    fix th e problem of naming

--- app/main/lib/prepare/index.ts
+++ app/main/lib/prepare/index.ts
@@ -8,8 +8,8 @@ import { updateYtDlp } from "./updateYtdlp";
 export async function PrePare() {
   await PrePareScripts(); //yt-dlp is downloaded by the Scripts itself
   await DefinePlugins();
+  await updateYtDlp();
 }
 export async function AfterLunch() {
   AfterLunchScripts();
-  updateYtDlp();
 }
